89a6c74dd0f3a9fa57727d54b773588391752f96
Marc Calvet Palao <marc200102@gmail.com>
2025-11-28 10:54:22 +0100

    fix: azure functions

--- api/postRanking/index.ts
+++ api/postRanking/index.ts
@@ -2,7 +2,6 @@ import { app, HttpRequest, HttpResponseInit } from "@azure/functions";
 
 export async function postRankingHandler(req: HttpRequest): Promise<HttpResponseInit> {
   const body = await req.json();
-  
   return {
     status: 200,
     jsonBody: { received: body }
