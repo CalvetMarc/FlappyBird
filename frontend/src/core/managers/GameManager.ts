afd22e9d7e5ed9875216075677d588a8c69b2cf4
Marc Calvet Palao <marc200102@gmail.com>
2025-11-28 19:51:18 +0100

    eefew

--- frontend/src/core/managers/GameManager.ts
+++ frontend/src/core/managers/GameManager.ts
@@ -28,7 +28,7 @@ export class GameManager extends SingletonBase<GameManager> {
   private constructor() {
     super();
     this.mousePos = new Point(0,0);
-    this.sessionData = { lastScore: 0, lastGameTime: 0, name: "Uri" };
+    this.sessionData = { lastScore: 0, lastGameTime: 0, name: "Anna" };
     this.settings = { audioEnabled: true, dayCycleEnabled: true, speedRampEnabled: false };
   }
 
