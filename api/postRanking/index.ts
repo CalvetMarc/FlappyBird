258d578ecf3b2bf1f86e2f6d831756f5035066c9
Marc Calvet Palao <marc200102@gmail.com>
2025-11-28 13:48:15 +0100

    try function

--- api/postRanking/index.ts
+++ api/postRanking/index.ts
@@ -1,24 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
 const postRanking: AzureFunction = async function (context: Context, req: HttpRequest) {
-  
-  const allowedOrigin = "https://white-pebble-036454303.3.azurestaticapps.net";
 
-  const origin = req.headers["origin"] || req.headers["referer"];
-
-  if (!origin || !origin.startsWith(allowedOrigin)) {
-    context.res = {
-      status: 401,
-      body: { error: "Unauthorized request" }
-    };
-    return;
-  }
-
-  const data = req.body;
+  const body = req.body;
 
   context.res = {
     status: 200,
-    body: { ok: true, received: data }
+    body: { ok: true, received: body }
   };
 };
 
--- frontend/src/SessionManager.ts
+++ frontend/src/SessionManager.ts
@@ -1,15 +1,16 @@
 export async function sendScore(sessionData: { name: string; lastScore: number; lastGameTime: number;}) {
   try {
-    const res = await fetch("/api/postRanking", {
+    const res = await fetch("/.auth/function/call/postRanking", {
       method: "POST",
-      headers: {
+      headers: { 
         "Content-Type": "application/json"
       },
       body: JSON.stringify(sessionData)
     });
 
-    const json = await res.json();
-    console.log("SERVER RESPONSE:", json);
+    const out = await res.json();
+    console.log("SERVER RESPONSE:", out);
+
   } catch (err) {
     console.error("Error enviant puntuació:", err);
   }
