4c3d337b009fe105e2c6e42cebb34d23d9252947
Marc Calvet Palao <marc200102@gmail.com>
2025-11-30 17:23:44 +0100

    bug

--- frontend/src/core/objects/UI/EditableField.ts
+++ frontend/src/core/objects/UI/EditableField.ts
@@ -1,10 +1,18 @@
-import { Container, BitmapText, Point, Graphics, Size } from "pixi.js";
+import { Container, Point, Graphics } from "pixi.js";
 import { AssetsManager } from "../../managers/AssetsManager";
 
 export class EditableField extends Container {
   private background: Graphics;
   private htmlInput!: HTMLInputElement;
   repositionEvent!: () => void;
+
+  // Handlers guardats per poder desregistrar correctament
+  private onPointerOver!: () => void;
+  private onPointerOut!: () => void;
+
+  private onInputHandler!: () => void;
+  private onFocusHandler!: () => void;
+  private onBlurHandler!: () => void;
 
   constructor(htmlInput: HTMLInputElement, background: Graphics, maxTextSize: number, repositionEvent: () => void) {
     super();
@@ -15,35 +23,46 @@ export class EditableField extends Container {
 
     this.background.eventMode = "static";
 
-    this.background.on("pointerover", () => this.onMouseEnter());
-    this.background.on("pointerout", () => this.onMouseExit());
+    // ---------- HANDLERS PIXI ----------
+    this.onPointerOver = () => this.onMouseEnter();
+    this.onPointerOut = () => this.onMouseExit();
 
-    this.htmlInput.addEventListener("input", () => {
+    this.background.on("pointerover", this.onPointerOver);
+    this.background.on("pointerout", this.onPointerOut);
+
+    // ---------- HANDLERS HTML ----------
+    this.onInputHandler = () => {
       htmlInput.value = htmlInput.value.slice(0, maxTextSize);
       this.repositionEvent();
-    });
+    };
 
-    this.htmlInput.addEventListener("blur", () => {
-        if(htmlInput.value.length <= 0){
-          htmlInput.value = "Guest";
-          this.repositionEvent();
-        }
-    });
-    
-    
+    this.onFocusHandler = () => {};
+
+    this.onBlurHandler = () => {
+      if (htmlInput.value.length <= 0) {
+        htmlInput.value = "Guest";
+        this.repositionEvent();
+      }
+    };
+
+    this.htmlInput.addEventListener("input", this.onInputHandler);
+    this.htmlInput.addEventListener("focus", this.onFocusHandler);
+    this.htmlInput.addEventListener("blur", this.onBlurHandler);
   }
 
-  public refreshVisuals(){
+  public refreshVisuals() {
     this.repositionEvent();
   }
 
   public freeResources(): void {
-    this.background.off("pointerover");
-    this.background.off("pointerout");
+    // PIXI
+    this.background.off("pointerover", this.onPointerOver);
+    this.background.off("pointerout", this.onPointerOut);
 
-    this.htmlInput.removeEventListener("input", this.repositionEvent);
-    this.htmlInput.removeEventListener("focus", () => {});
-    this.htmlInput.removeEventListener("blur", () => {});
+    // HTML
+    this.htmlInput.removeEventListener("input", this.onInputHandler);
+    this.htmlInput.removeEventListener("focus", this.onFocusHandler);
+    this.htmlInput.removeEventListener("blur", this.onBlurHandler);
 
     if (this.htmlInput.parentElement) {
       this.htmlInput.parentElement.removeChild(this.htmlInput);
@@ -52,10 +71,10 @@ export class EditableField extends Container {
     this.background.removeChildren();
     AssetsManager.I.releaseSprite(this.background);
 
+    // Reset referències
     this.htmlInput = null!;
     this.repositionEvent = () => {};
   }
-
 
   private onMouseEnter() {
     this.refreshVisuals();
@@ -65,5 +84,4 @@ export class EditableField extends Container {
   private onMouseExit() {
     this.background.alpha = 1;
   }
-
 }
