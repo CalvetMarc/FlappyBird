77f50201b574828fd39030fc6c91bccc32479b5e
Marc Calvet Palao <marc200102@gmail.com>
2025-12-04 13:49:56 +0100

    prevent zoom

--- frontend/src/core/managers/GameManager.ts
+++ frontend/src/core/managers/GameManager.ts
@@ -79,9 +79,24 @@ export class GameManager extends SingletonBase<GameManager> {
       }
     });
 
-    window.addEventListener("gesturestart", (e) => e.preventDefault());
-    window.addEventListener("gesturechange", (e) => e.preventDefault());
-    window.addEventListener("gestureend", (e) => e.preventDefault());
+    document.addEventListener('touchstart', function(e) {
+      if (e.touches.length > 1) {
+        e.preventDefault();
+      }
+    }, { passive: false });
+
+    let lastTouchEnd = 0;
+    document.addEventListener('touchend', function (e) {
+      const now = Date.now();
+      if (now - lastTouchEnd <= 300) {
+        e.preventDefault(); 
+      }
+      lastTouchEnd = now;
+    }, false);
+
+    document.addEventListener("gesturestart", (e) => e.preventDefault());
+    document.addEventListener("gesturechange", (e) => e.preventDefault());
+    document.addEventListener("gestureend", (e) => e.preventDefault());
   }
 
 
